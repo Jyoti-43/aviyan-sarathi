756433c2e5af86503ccf90f114af584e3cc57939
Jyoti Joshi <joshijyoti38021@gmail.com>
2024-05-20 16:04:19 +0545

    minor change

---
+++ src/Components/Stories/StoriesPage.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+
+const StoriesPage = () => {
+  return (
+    <>
+<section class="category-section">
+        <div class="container" data-aos="fade-up">
+
+          <div class="section-header d-flex justify-content-between align-items-center mb-5">
+           <span><h2>Stories</h2></span> <span className="ml-2"><Link to="/stories" ><h4 >View all</h4></Link></span>
+            <div>
+              <h1 className='latest'>
+              All Stories
+              </h1>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-9 ">
+              {/* yo div ma hamro video with additional content   (data fetch garnu parne thau) */}
+              <div class="d-lg-flex post-entry-2">
+             
+                 <div class="me-3 thumbnail mb-4 mb-lg-0 d-inline-block">
+                      <ReactPlayer
+                            url="https://www.youtube.com/watch?v=qEB4Jjmfjhk"
+                            width="93%"
+                            height="87%"
+                            controls
+                          />
+                  {/* <img src="assets/img/post-landscape-6.jpg" alt="" class="img-fluid" /> */}
+                </div> 
+                <div>
+                  <div class="post-meta"><span class="date">Culture</span> <span class="mx-1"> </span> <span>{currentDate}</span></div>
+                  <div class="d-flex align-items-center author">
+                    <div class="photo"><img src="assets/img/balaramThapa.jpg" alt="" class="img-fluid" /></div>
+                    <div class="name">
+                      <h3 class="m-0 p-0">Mr. Balaram Thapa</h3>
+                    </div>
+                  </div>
+                  <h3 className=" m-0"><Link to="./storyDetails" >Know the Story of First Cycle Traveller from Purva to Mechi.</Link></h3>
+                  <p>Mr. Balaram Thapa's remarkable journey began as the first cyclist to traverse the entire nation, marking a significant milestone in his life.</p>
+                  
+                </div>
+              </div>
+              {/* div sakinxa */}
+
+              <div class="d-lg-flex post-entry-2">
+             
+             <div class="me-3 thumbnail mb-4 mb-lg-0 d-inline-block">
+                  <ReactPlayer
+                        url="https://www.youtube.com/watch?v=Aj_hnge3vBA"
+                        width="93%"
+                        height="87%"
+                        controls
+                      />
+             
+            </div> 
+            <div >
+              <div class="post-meta"><span class="date">Culture</span> <span class="mx-1"> </span> <span>{currentDate}</span></div>
+              <div class="d-flex align-items-center author">
+                <div class="photo"><img src="assets/img/puranShakya.jpg" alt="" class="img-fluid" /></div>
+                <div class="name">
+                  <h3 class="m-0 p-0">Puran Shakya </h3>
+                </div>
+              </div>
+              
+              <h3 className=" m-0">< Link to="/storyDetails">Born in 1956, Shakya learned traditional painting from Siddhi Muni Shakya (1979 – 1982).."
+   </Link> </h3>
+              <p> He specialized in mineral color mixing and mudras of different deities. </p>
+             
+            </div>
+           
+          </div>
+
+             
+            </div>
+            
+
+            <div className="col-md-3">
+                <div className="aside-block">
+                  
+                  <div className="tab-content" id="pills-tabContent">
+                    <div className="tab-pane fade show active" id="pills-latest" role="tabpanel" aria-labelledby="pills-latest-tab">
+                      {/* maping of data */}
+                      {sortedPosts.map((post, index) => (
+                        <div
+                          key={index}
+                          className="post-entry-1 border-bottom"
+                          // onClick={() => handlePostClick(index)}
+                        >
+                          <div className="post-meta ">
+                            <span className="mx-1"></span>{' '}
+                            <span className="date">{post.date}</span>
+                          </div>
+                          <h2 className="mb-2">
+                            <Link to ="/storyDetails">{post.title}</Link>
+                          </h2>
+                          <span className="author mb-3 d-block">{post.author}</span>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                
+              </div>           
+            
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default StoriesPage
